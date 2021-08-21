eebf41bce0ae6489972a7ce5bb5eb97dc61e705c
andrei aliashkevich <aliashkevich01@gmail.com>
2021-08-21 11:46:26 +0300

    feat: update solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (array === undefined || array.length === 0 ) {
+        return 0;
+    }
+    let min = Math.min( ...array );
+    return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array === undefined || array.length === 0 ) {
+        return 0;
+    }
+    let max = Math.max( ...array );
+    return max;
 }
-
 exports.avg = function avg (array) {
-  return 0;
+    if (array === undefined || array.length === 0 ) {
+        return 0;
+    }
+    let aver=0;
+     for(let i=0;i<array.length;i++){
+       aver+=array[i];
+     }
+     return aver/array.length;
 }
